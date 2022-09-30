afc4ebfc29295213a0ca7f203a2787da5b486173
judenfors <rudenfors.eric@gmail.com>
2022-09-30 10:31:05 +0200

    Add project files.

---
+++ Uppgift3._14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift3._14
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara j för ja och n för nej");
+            string rättsvar =Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+
+            
+            if(ålder <= 22 & rättsvar=="j")
+            {
+                Console.WriteLine("Vi vill anställa dig");
+
+            }
+            else{
+                Console.WriteLine("Vi vill inte anställa dig");
+            }
+        }
+    }
+}
+
